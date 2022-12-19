12f1bb4a2493f3f53bf589ac91f326fa2e90d85c
Cyanea <g3tsuzumi@gmail.com>
2022-12-19 11:15:44 +0900

    daily push

---
+++ sanity.js
@@ -0,0 +1,12 @@
+import {
+    createClient,
+    } from 'next-sanity'
+
+    export const config = {
+        projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+        dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+        apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+        useCdn: process.env.NODE_ENV === 'production',
+    }
+
+export const sanityClient = createClient(config)
