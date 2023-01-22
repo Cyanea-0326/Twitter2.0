d4025289fed8c02cd62053c4891d4e895194a9b7
shota <96759613+Cyanea-0326@users.noreply.github.com>
2023-01-23 02:17:25 +0900

    Update SidebarRow.tsx

--- components/SidebarRow.tsx
+++ components/SidebarRow.tsx
@@ -9,7 +9,7 @@ interface Props {
 function SidebarRow({ Icon, title }: Props) {
   return (
     <div
-    onClick={() => onClick?.()}
+    //onClick={() => onClick?.()}
     className="group flex max-w-fit cursor-pointer items-cetner space-x-2 
     rounded-full px-4 py-3 transition-all duration-200
     hover:bg-gray-100 group">
